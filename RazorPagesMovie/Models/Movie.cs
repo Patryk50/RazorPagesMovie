16214a43b8886b4761367ffc6ea5a73a1a873023
patryk.kordel@gmail.com <patryk.kordel@gmail.com>
2023-05-04 13:38:13 +0200

    Dodaj pliki projektów.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required, Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required, Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Required, RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Required, RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
